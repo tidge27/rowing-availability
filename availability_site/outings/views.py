1094fafdc9849babb50ce6252593ff07b7e70eaa
Thomas Garry <thomasgarry@thomass-mbp.gateway.sonic.net>
2018-09-04 23:28:01 -0700

    skip weeks

--- availability_site/outings/views.py
+++ availability_site/outings/views.py
@@ -3,6 +3,7 @@ from outings.models import Event
 import datetime
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from dateutil.relativedelta import relativedelta, MO
 
 def availabilities(request):
     user = request.user
@@ -11,8 +12,11 @@ def availabilities(request):
         week_commencing = datetime.datetime.strptime(request.GET.get("date", ""), "%d%m%Y")
     except Exception as e:
         now = datetime.datetime.now()
-        week_commencing = datetime.datetime(now.year, now.month, now.day, 0, 0, 0)
-
+        week_commencing = datetime.datetime(now.year, now.month, now.day, 0, 0, 0) + relativedelta(weekday=MO(-1))
+    prev_monday = datetime.datetime.strftime(week_commencing + relativedelta(weekday=MO(-2)), "%d%m%Y")
+    prev_monday_link = "?date={}".format(prev_monday)
+    next_monday = datetime.datetime.strftime(week_commencing + relativedelta(weekday=MO(2)), "%d%m%Y")
+    next_monday_link = "?date={}".format(next_monday)
 
     if request.method == "POST":
     # if False:
@@ -23,9 +27,23 @@ def availabilities(request):
             formsets.append(BookFormSet(request.POST, request.FILES, prefix="set_{}".format(i), instance=user,
                               queryset=Event.objects.filter(start_time__gte=start_of_day, start_time__lte=end_of_day),
                               date=week_commencing+ datetime.timedelta(days=i)))
+        all_valid = True
         for formset in formsets:
             if formset.is_valid():
+                pass
+            else:
+                all_valid = False
+        if all_valid:
+            for formset in formsets:
                 formset.save()
+            formsets = []
+            for i in range(0, 6):
+                start_of_day = week_commencing + datetime.timedelta(days=i)
+                end_of_day = week_commencing + datetime.timedelta(days=i + 1)
+                formsets.append(BookFormSet(prefix="set_{}".format(i), instance=user,
+                                            queryset=Event.objects.filter(start_time__gte=start_of_day,
+                                                                          start_time__lte=end_of_day),
+                                            date=week_commencing + datetime.timedelta(days=i)))
             # Do something. Should generally end with a redirect. For example:
             # return HttpResponseRedirect('')
     else:
@@ -39,7 +57,10 @@ def availabilities(request):
                                         date=week_commencing+ datetime.timedelta(days=i)))
 
     template = loader.get_template('outings/availabilities.html')
-    context = {'formsets': formsets}
+    context = {'formsets': formsets,
+               'prev_monday_link': prev_monday_link,
+               'next_monday_link': next_monday_link,
+               }
     return HttpResponse(template.render(context, request))
 
 # def index(request):
